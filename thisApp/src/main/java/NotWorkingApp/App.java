1cc18343a0513fe352673da6645f19b03aa570c1
minchdavidm <minchdavidm@email.arizona.edu>
2019-02-05 15:48:44 -0700

    Trying to get it working

---
+++ thisApp/src/main/java/NotWorkingApp/App.java
@@ -0,0 +1,13 @@
+package NotWorkingApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
